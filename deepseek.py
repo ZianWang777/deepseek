c126bb576458e73fcb2c55de1655b22f2dc68d11
ZianWang777 <vbqf1090@leeds.ac.uk>
2025-02-06 23:55:05 +0000

    Update deepseek.py

--- deepseek.py
+++ deepseek.py
@@ -1,16 +1,16 @@
-pip3 install openai
 
 from openai import OpenAI
+client = OpenAI()
 
-client = OpenAI(api_key="sk-be72f955067040499a6e1be16bf096c8", base_url="https://api.deepseek.com")
-
-response = client.chat.completions.create(
-    model="deepseek-chat",
+completion = client.chat.completions.create(
+    model="gpt-4o-mini",
     messages=[
-        {"role": "system", "content": "You are a helpful assistant"},
-        {"role": "user", "content": "Hello"},
-    ],
-    stream=False
+        {"role": "system", "content": "You are a helpful assistant."},
+        {
+            "role": "user",
+            "content": "Write a haiku about recursion in programming."
+        }
+    ]
 )
 
-print(response.choices[0].message.content)
+print(completion.choices[0].message)
