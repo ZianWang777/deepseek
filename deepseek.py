9c638e29dc1c74ac04d7dc7f9cb220af74b559c2
ZianWang777 <vbqf1090@leeds.ac.uk>
2025-02-06 22:39:43 +0000

    Update deepseek.py

--- deepseek.py
+++ deepseek.py
@@ -2,7 +2,7 @@ pip3 install openai
 
 from openai import OpenAI
 
-client = OpenAI(api_key="<DeepSeek API Key>", base_url="https://api.deepseek.com")
+client = OpenAI(api_key="sk-be72f955067040499a6e1be16bf096c8", base_url="https://api.deepseek.com")
 
 response = client.chat.completions.create(
     model="deepseek-chat",
